4dfebcfd804778e15d4c3d48c1c852e874421569
leoni <kylakov_leonid@mail.ru>
2024-05-13 17:39:39 +0300

    FirstCommit

--- src/main/java/web/exception/InputDataException.java
+++
@@ -1,7 +0,0 @@
-package web.exception;
-
-public class InputDataException extends RuntimeException {
-    public InputDataException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,29 @@
+package web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private Byte age;
+
+    @Column(nullable = false)
+    private String city;
+
+}
